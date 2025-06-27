46ef8201bf22b29d9988b2f29d91d90d6ff0f298
MrPickering <mark.the.pickering@gmail.com>
2025-06-27 15:13:51 -0700

    Update Game.tsx

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import GameBoard from "./GameBoard";
 
 export default function Game() {
@@ -6,30 +5,3 @@ export default function Game() {
   // Initialization is handled by the parent GameContainer
   return <GameBoard />;
 }
-=======
-import { useEffect } from "react";
-import { useGameState } from "../lib/stores/useGameState";
-import TopicSelection from "./TopicSelection";
-import GameBoard from "./GameBoard";
-import GameUI from "./GameUI";
-
-export default function Game() {
-  const { gamePhase, initializeGame } = useGameState();
-
-  useEffect(() => {
-    initializeGame();
-  }, [initializeGame]);
-
-  return (
-    <>
-      {gamePhase === 'topic_selection' && <TopicSelection />}
-      {(gamePhase === 'playing' || gamePhase === 'paused' || gamePhase === 'game_over') && (
-        <>
-          <GameBoard />
-          <GameUI />
-        </>
-      )}
-    </>
-  );
-}
->>>>>>> 8dfa9d15082776f0965b94f8640826bd7e21a1e5
